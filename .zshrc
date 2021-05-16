f801ee40acc5b61e4b4d198355a057ae5f9357ed
Sertaç Özercan <852750+sozercan@users.noreply.github.com>
2021-05-16 04:57:50 +0000

    update

--- .zshrc
+++ .zshrc
@@ -1,47 +1,106 @@
+# You may need to manually set your language environment
+export LANG="en_US.UTF-8"
+export LC_CTYPE="en_US.UTF-8"
+export LC_ALL="en_US.UTF-8"
+export GPG_TTY=$(tty)
+
+# Preferred editor for local and remote sessions
+export EDITOR='nano'
+
+# Path to your oh-my-zsh installation.
+export ZSH="$HOME/.oh-my-zsh"
+
+# Updated Path Exports
+export PATH="/usr/local/sbin:$PATH"
+
+# Set name of the theme to load --- if set to "random", it will
+# load a random theme each time oh-my-zsh is loaded, in which case,
+# to know which specific one was loaded, run: echo $RANDOM_THEME
+# See https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
+ZSH_THEME="powerlevel10k/powerlevel10k"
+
+# Set list of themes to pick from when loading at random
+# Setting this variable when ZSH_THEME=random will cause zsh to load
+# a theme from this variable instead of looking in ~/.oh-my-zsh/themes/
+# If set to an empty array, this variable will have no effect.
+# ZSH_THEME_RANDOM_CANDIDATES=( "robbyrussell" "agnoster" )
+
+# Uncomment the following line to use case-sensitive completion.
+# CASE_SENSITIVE="true"
+
+# Uncomment the following line to use hyphen-insensitive completion.
+# Case-sensitive completion must be off. _ and - will be interchangeable.
+# HYPHEN_INSENSITIVE="true"
+
+# Uncomment the following line to disable bi-weekly auto-update checks.
+# DISABLE_AUTO_UPDATE="true"
+
+# Uncomment the following line to automatically update without prompting.
+DISABLE_UPDATE_PROMPT="true"
+
+# Uncomment the following line to change how often to auto-update (in days).
+# export UPDATE_ZSH_DAYS=13
+
+# Uncomment the following line if pasting URLs and other text is messed up.
+# DISABLE_MAGIC_FUNCTIONS=true
+
+# Uncomment the following line to disable colors in ls.
+# DISABLE_LS_COLORS="true"
+
+# Uncomment the following line to disable auto-setting terminal title.
+# DISABLE_AUTO_TITLE="true"
+
+# Uncomment the following line to enable command auto-correction.
+# ENABLE_CORRECTION="true"
+
+# Uncomment the following line to display red dots whilst waiting for completion.
+COMPLETION_WAITING_DOTS="true"
+
+# Uncomment the following line if you want to disable marking untracked files
+# under VCS as dirty. This makes repository status check for large repositories
+# much, much faster.
+# DISABLE_UNTRACKED_FILES_DIRTY="true"
+
+# Uncomment the following line if you want to change the command execution time
+# stamp shown in the history command output.
+# You can set one of the optional three formats:
+# "mm/dd/yyyy"|"dd.mm.yyyy"|"yyyy-mm-dd"
+# or set a custom format using the strftime function format specifications,
+# see 'man strftime' for details.
+# HIST_STAMPS="mm/dd/yyyy"
+
+# Which plugins would you like to load?
+# Standard plugins can be found in ~/.oh-my-zsh/plugins/*
+# Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
+# Example format: plugins=(rails git textmate ruby lighthouse)
+# Add wisely, as too many plugins slow down shell startup.
+plugins=(
+	git
+	zsh-syntax-highlighting
+	zsh-autosuggestions
+)
+
+# Loads Homebrew (only if exists and needed (Linux))
+[[ ! -f /home/linuxbrew/.linuxbrew/bin/brew ]] || eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+
+# Load GitHub CLI Completion
+eval "$(gh completion -s zsh)"
+
 # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
 # Initialization code that may require console input (password prompts, [y/n]
-# confirmations, etc.) must go above this block, everything else may go below.
+# confirmations, etc.) must go above this block; everything else may go below.
 if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
   source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
 fi
 
-# Lines configured by zsh-newuser-install
-HISTFILE=~/.histfile
-HISTSIZE=10000
-SAVEHIST=1000
-setopt autocd extendedglob
-bindkey -e
-# End of lines configured by zsh-newuser-install
-# The following lines were added by compinstall
-zstyle :compinstall filename '/home/sozercan/.zshrc'
+# Custom Aliases
+alias bu="brew upgrade"
 
-autoload -Uz compinit
-compinit
-# End of lines added by compinstall
-source ~/.powerlevel10k/powerlevel10k.zsh-theme
+# only macOS supports casks
+[[ -f /home/linuxbrew/.linuxbrew/bin/brew ]] || alias bu="brew upgrade && brew cask upgrade"
 
-eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
-alias k=kubectl
-alias bu="brew update && brew upgrade && brew cleanup"
-alias -g ...='../..'
-alias -g ....='../../..'
-alias -g .....='../../../..'
-alias -g ......='../../../../..'
-
-export GOPATH=$HOME/go
-export PATH="$GOPATH/bin:$PATH"
-export PATH="/usr/local/bin:$PATH"
-export DOCKER_BUILDKIT=1
-export DOCKER_CLI_EXPERIMENTAL=enabled
-
-bindkey "^[[1;5C" forward-word
-bindkey "^[[1;5D" backward-word
-
-# source /home/sozercan/.zsh-autocomplete/zsh-autocomplete.plugin.zsh
-source /home/linuxbrew/.linuxbrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-source /home/linuxbrew/.linuxbrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh
-source /home/linuxbrew/.linuxbrew/share/zsh-history-substring-search/zsh-history-substring-search.zsh
-source /home/linuxbrew/.linuxbrew/share/zsh-navigation-tools/zsh-navigation-tools.plugin.zsh
+# Load Oh My ZSH
+source $ZSH/oh-my-zsh.sh
 
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
--- install.sh
+++ install.sh
@@ -1,22 +1,140 @@
-#!/bin/bash
+#!/usr/bin/env zsh
 
-set -xueEo pipefail
+echo "\e[31m\n\n!! WARNING: This script will overwrite existing ZSH, Bash Profile and Oh-My-ZSH configurations !! \n"
+echo "!! WARNING: Please do not open other terminal session until the scripts finishes !! \e[39m\n"
 
-sudo apt update
-sudo apt install -y build-essential curl file git
-#sudo apt upgrade -y
-sudo apt autoremove -y
-sudo apt autoclean
+case "$(uname -s)" in
+    Darwin)
+        echo "\e[32m[DOT]\e[33m Darwin based environment detected ... \e[39m\n"
+    ;;
+    Linux)
+        if [ "$(grep -Ei 'debian|buntu|mint' /etc/*release)" ]; then
+            echo "\e[32m[DOT]\e[33m Debian based environment detected ... \e[39m\n"
+            # install required dependencies
+            echo "\e[32m[DOT]\e[34m installing packages ... \e[39m\n"
+            sudo apt -y install build-essential git debconf locales > /dev/null 2>&1
+            # generate utf-8 environment
+            echo "\e[32m[DOT]\e[34m generating locales ... \e[39m\n"
+            sudo locale-gen --purge en_US.UTF-8 > /dev/null 2>&1
+        # check if environment is fedora/redhat
+        elif [ "$(grep -Ei 'fedora|redhat|centos' /etc/*release)" ]; then
+            echo "\e[32m[DOT]\e[33m RedHat based environment detected ... \e[39m\n"
+            # install required dependencies
+            echo "\e[32m[DOT]\e[34m installing packages ... \e[39m\n"
+            sudo dnf install @development-tools git -y > /dev/null 2>&1
+            # generate utf-8 environment
+            echo "\e[32m[DOT]\e[34m generating locales ... \e[39m\n"
+            localedef -v -c -i en_US -f UTF-8 en_US.UTF-8 > /dev/null 2>&1
+        fi
+    ;;
+    CYGWIN*|MINGW32*|MSYS*|MINGW*)
+        echo "\e[32m[DOT]\e[33m Windows based environment detected ... \e[39m\n"
+        echo "\e[32m[DOT]\e[31m This is not a supported environment. Exiting. \e[39m\n"
+        exit 1
+     ;;
+    *)
+        echo "\e[32m[DOT]\e[31m Unable to detected the environment! \e[39m\n"
+        echo "\e[32m[DOT]\e[31m This is not a supported environment. Exiting. \e[39m\n"
+        exit 1
+    ;;
+esac
 
+# exports the language definitions
+echo "\e[32m[DOT]\e[34m exporting locales ... \e[39m\n"
+export LANG="en_US.UTF-8"
+export LC_CTYPE="en_US.UTF-8"
+export LC_ALL="en_US.UTF-8"
+
+# tells homebrew to do a silent install
+export CI=1
+
+# delete previous installations
+echo "\e[32m[DOT]\e[34m deleting previous installation of this dotfiles ... \e[39m\n"
+rm -rf ~/.oh-my-zsh/ ~/.zshrc ~/.bash_profile ~/.p10k.zsh > /dev/null 2>&1
+
+# copies base bash profile
+echo "\e[32m[DOT]\e[34m copying bash profile file ... \e[39m\n"
+cp -rf .bash_profile ~/.bash_profile > /dev/null 2>&1
+
+# install the homebrew (if stdin is available requires confirmation)
+echo "\e[32m[DOT]\e[34m installing homebrew ...  \e[39m\n"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
-test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
-test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
-echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
 
-brew install zsh zsh-autosuggestions zsh-history-substring-search zsh-navigation-tools zsh-syntax-highlighting
+case "$(uname -s)" in
+    Darwin)
+        echo "\e[32m[DOT]\e[34m installing nerd fonts ... \e[39m\n"
+        brew tap homebrew/cask-fonts > /dev/null 2>&1
 
-brew install romkatv/powerlevel10k/powerlevel10k
-echo 'source /usr/local/opt/powerlevel10k/powerlevel10k.zsh-theme' ~/.zshrc >>!
+        brew install font-hack-nerd-font > /dev/null 2>&1
+    ;;
+    Linux)
+        echo "\e[32m[DOT]\e[34m configuring homebrew ... \e[39m\n"
 
-brew install go kubernetes-cli kustomize azure-cli jq helm kind git
+        # installs homebrew on the environment
+        test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
+        test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+        test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
+        echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
+
+        echo "\e[32m[DOT]\e[34m installing nerd fonts ... \e[39m\n"
+        # creates local fonts directory
+        mkdir -p ~/.local/share/fonts
+
+        (
+            # goes to the directory
+            cd ~/.local/share/fonts
+
+            # downloads the font file
+            curl -fLo "Hack NF.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/Regular/complete/Hack%20Regular%20Nerd%20Font%20Complete%20Mono.ttf > /dev/null 2>&1
+
+            # reloads font cache
+            echo "\e[32m[DOT]\e[34m rebuilding fonts ... \e[39m\n"
+            fc-cache -f -v > /dev/null 2>&1
+        )
+    ;;
+    *)
+    ;;
+esac
+
+echo "\e[32m[DOT]\e[34m installing homebrew packages ... \e[39m\n"
+
+# installs all the required packages
+brew install go kubernetes-cli kustomize jq helm kind git git-lfs gh wget > /dev/null 2>&1
+
+# configures git lfs
+echo "\e[32m[DOT]\e[34m configuring git lfs ... \e[39m\n"
+git lfs install --system > /dev/null 2>&1
+
+# updates git configuration
+echo "\e[32m[DOT]\e[34m enabling default git strategies ... \e[39m\n"
+git config --global pull.rebase true
+
+# installs oh-my-zsh
+echo "\e[32m[DOT]\e[34m installing oh my zsh ... \e[39m\n"
+! sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended > /dev/null 2>&1
+
+echo "\e[32m[DOT]\e[34m installing oh my zsh plugins ... \e[39m\n"
+
+# installs power-level-10k
+! git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k" > /dev/null 2>&1
+
+# installs zsh auto suggestions
+! git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" > /dev/null 2>&1
+
+# installs zsh-syntax-highlighting
+! git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting" > /dev/null 2>&1
+
+echo "\e[32m[DOT]\e[34m copying environment files ... \e[39m\n"
+
+# copies the ZSH environment file
+cp -rf .zshrc ~/.zshrc > /dev/null 2>&1
+
+# copies the PowerLevel10K configuration file
+cp -rf .p10k.zsh ~/.p10k.zsh > /dev/null 2>&1
+
+# loads the source
+source ~/.zshrc
+
+echo "\e[32mInstallation Finished. Exiting. \e[39m\n"
+
+exit 0
